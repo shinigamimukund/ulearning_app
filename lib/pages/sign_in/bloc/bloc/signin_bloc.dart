f056ab695257ef9ff4fea8bbda9519df1e392b96
shinigamimukund <shinigamimukund@gmail.com>
2023-05-08 17:59:05 +0530

    4. on() event => emit to signin_state

--- lib/pages/sign_in/bloc/bloc/signin_bloc.dart
+++ lib/pages/sign_in/bloc/bloc/signin_bloc.dart
@@ -9,19 +9,21 @@ class SigninBloc extends Bloc<SigninEvent, SigninState> {
   ///super should be the class with Initial state
   SigninBloc() : super(const SigninInitial()) {
     on<SigninEvent>((event, emit) {
-
       ///{@start event methods}
       ///this is where we write [event handling code]
       ///there are [2] methods here -
       ///`on()` and `(event,emit){}` methods
       ///`on()` method takes `(event,emit){}` as [parameter]
+      ///{@start event methods}
       on<EmailEvent>((event, emit) {
-        // TODO: implement event handler
+        ///-------we can write it like below also but,
+        ///-------we need to pass only email so
+        ///[emit(state.copyWith(email: state.email, password: state.password));]
+        emit(state.copyWith(email: state.email));
       });
       on<PasswordEvent>((event, emit) {
-        // TODO: implement event handler
-      }); 
-      ///{@start event methods}
+        emit(state.copyWith(password: state.password));
+      });
     });
   }
 }
