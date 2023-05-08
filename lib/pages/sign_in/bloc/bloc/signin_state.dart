7b7020ffc5d420fe2b1795d0f6bee567ebd74e70
shinigamimukund <shinigamimukund@gmail.com>
2023-05-08 12:06:55 +0530

    3. copyWith function with ?? operator

--- lib/pages/sign_in/bloc/bloc/signin_state.dart
+++ lib/pages/sign_in/bloc/bloc/signin_state.dart
@@ -2,12 +2,25 @@ part of 'signin_bloc.dart';
 
 ///State bloc contains all the `Variable` which holds the state
 @immutable
-abstract class SigninState {
+class SigninState {
   final String? email;
   final String? password;
-  const SigninState({this.email, this.password });
+  const SigninState({this.email, this.password});
+
+  SigninState copyWith({String? email, String? password}) {
+    return SigninState(
+      ///{@start ??}
+      ///if copy email is empty this.email is assigned that is -
+      ///  if no value is passed then "" empty value is assigned
+      ///print(1 ?? 3); // <-- Prints 1.
+      ///print(null ?? 12); // <-- Prints 12.
+      //////{@end ??}
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
 }
 
 class SigninInitial extends SigninState {
-  const SigninInitial():super(email : "", password : "");
+  const SigninInitial() : super(email: "", password: "");
 }
