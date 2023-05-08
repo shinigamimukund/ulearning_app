2f29b3f23f25136d859412ee69df3e270e817fe2
shinigamimukund <shinigamimukund@gmail.com>
2023-05-08 10:40:03 +0530

    2.InitialState-trying out stuff

--- lib/pages/sign_in/bloc/bloc/signin_state.dart
+++ lib/pages/sign_in/bloc/bloc/signin_state.dart
@@ -1,8 +1,13 @@
 part of 'signin_bloc.dart';
 
-
 ///State bloc contains all the `Variable` which holds the state
 @immutable
-abstract class SigninState {}
+abstract class SigninState {
+  final String? email;
+  final String? password;
+  const SigninState({this.email, this.password });
+}
 
-class SigninInitial extends SigninState {}
+class SigninInitial extends SigninState {
+  const SigninInitial():super(email : "", password : "");
+}
