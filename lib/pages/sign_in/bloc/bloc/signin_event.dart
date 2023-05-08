22526216360b8adf8f8631c92abbfa75d3df8768
shinigamimukund <shinigamimukund@gmail.com>
2023-05-08 17:41:59 +0530

    3.structure of class in events and state

--- lib/pages/sign_in/bloc/bloc/signin_event.dart
+++ lib/pages/sign_in/bloc/bloc/signin_event.dart
@@ -1,7 +1,9 @@
 part of 'signin_bloc.dart';
 
 
-
+///single class [SigninEven] is inherited to all the classes 
+///because when we use the [SigninEvent] class in [SigninBloc]
+///all the sub classes(all the events) will be `passed` to [SigninBloc]
 @immutable
 abstract class SigninEvent {}
 
