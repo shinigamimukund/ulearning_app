2914b9c85bc52f2f4ef72305829e319192ffbbf3
shinigamimukund <shinigamimukund@gmail.com>
2023-05-09 18:02:43 +0530

    read state value with help of CONTEX -control page

---
+++ lib/pages/sign_in/sigin_in_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ulearning_app/pages/sign_in/bloc/bloc/signin_bloc.dart';
+
+class SignInController {
+  final BuildContext context;
+  const SignInController({required this.context});
+
+  void handleSignIn(String type) {
+    try {
+      if (type == "email") {
+        ///to `read` the `state` value from [SigninBloc]
+        ///we can also `retrieve state value` like this----below code
+        ///[BlocProvider.of<SigninBloc>(context).state;]
+        final state = context.read<SigninBloc>().state;
+      }
+    } catch (e) {
+      //add catch block
+    }
+  }
+}
