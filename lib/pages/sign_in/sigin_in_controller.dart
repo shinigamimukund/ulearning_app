f2fa578943e31184f19bb835947ac8d51e19ce76
shinigamimukund <shinigamimukund@gmail.com>
2023-05-10 01:12:28 +0530

    credentials, Future was added cause off async

--- lib/pages/sign_in/sigin_in_controller.dart
+++ lib/pages/sign_in/sigin_in_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/bloc/signin_bloc.dart';
@@ -8,7 +9,7 @@ class SignInController {
   final BuildContext context;
   const SignInController({required this.context});
 
-  void handleSignIn(String type) {
+  Future<void> handleSignIn(String type) async {
     try {
       if (type == "email") {
         ///to `read` the `state` value from [SigninBloc]
@@ -24,6 +25,13 @@ class SignInController {
         }
         if (password!.isEmpty) {
           //using package to handle verification
+        }
+        try {
+          final credentials = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+                  email: emailAddress, password: password);
+        } catch (e) {
+          //
         }
       }
     } catch (e) {
