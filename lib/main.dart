374a94e595bb4f86231e18f66070bcad98b40374
shinigamimukund <shinigamimukund@gmail.com>
2023-05-12 09:31:45 +0530

    BlocProvide separation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app/app_blocks.dart';
 import 'package:ulearning_app/app_states.dart';
+import 'package:ulearning_app/pages/bloc_providers.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/bloc/signin_bloc.dart';
 import 'package:ulearning_app/pages/sign_in/sign_in.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_bloc.dart';
@@ -26,20 +27,8 @@ class MyApp extends StatelessWidget {
     ///should be do'nt at the top level of the tree
     return MultiBlocProvider(
       //this is how we create Multi block Provider
-      /**now we will use `lazy` to load the provider because
-       * always the `first` block is loaded `first` when the app starts so to
-       * avoid this we use `lazy` to load the `important` bloc
-       * [********Important**********]
-       * If `lazy` is `falsy` then that bloc loads first
-       * If `lazy` is `true`, that bloc loads `after` the bloc which are marked `lazy` as `false`
-      */
-      ///`lazy` is used tell which bloc is exicuted first
-      ///by default lazy is true
-      providers: [
-        BlocProvider(lazy: false, create: (context) => WelcomeBloc()),
-        BlocProvider(lazy: true, create: (context) => AppBlocs()),
-        BlocProvider(create: (context) => SigninBloc()),
-      ],
+
+      providers: AppBlocProviders.allBlocProviders,
       child: ScreenUtilInit(
         builder: (context, child) => MaterialApp(
           debugShowCheckedModeBanner: false,
---
+++ lib/pages/bloc_providers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ulearning_app/pages/sign_in/bloc/bloc/signin_bloc.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_bloc.dart';
+
+import '../app_blocks.dart';
+
+/**now we will use `lazy` to load the provider because
+       * always the `first` block is loaded `first` when the app starts so to
+       * avoid this we use `lazy` to load the `important` bloc
+       * [********Important**********]
+       * If `lazy` is `falsy` then that bloc loads first
+       * If `lazy` is `true`, that bloc loads `after` the bloc which are marked `lazy` as `false`
+      */
+///`lazy` is used tell which bloc is exicuted first
+///by default lazy is true
+class AppBlocProviders {
+  static get allBlocProviders => [
+        BlocProvider(lazy: false, create: (context) => WelcomeBloc()),
+        BlocProvider(lazy: true, create: (context) => AppBlocs()),
+        BlocProvider(create: (context) => SigninBloc()),
+      ];
+}
