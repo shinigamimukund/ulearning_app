7c6697d7a06636bac91745ae421ffcdb44cc6b04
shinigamimukund <shinigamimukund@gmail.com>
2023-05-18 18:58:10 +0530

    bottom navigation bar icons

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app/pages/application/widgets/application_widget.dart';
 
 class ApplicationPage extends StatefulWidget {
@@ -15,7 +16,28 @@ class _ApplicationPageState extends State<ApplicationPage> {
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
-          body: buildPage(0),
+          body: buildPage(1),
+          bottomNavigationBar: BottomNavigationBar(
+            elevation: 0,
+            items: [
+              BottomNavigationBarItem(
+                label: "Home",
+                icon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset("assets/icons/home.png"),
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "Search",
+                icon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset("assets/icons/search.png"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
