526207d9415a4b64c3c27a50554a03a3f22cc32f
shinigamimukund <shinigamimukund@gmail.com>
2023-05-18 08:35:23 +0530

    next ui

--- lib/pages/register/register_controller.dart
+++ lib/pages/register/register_controller.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulearning_app/pages/register/widgets/bloc/register_bloc.dart';
