57cf9000536934fba297080813bc688261e165c8
shinigamimukund <shinigamimukund@gmail.com>
2023-05-21 10:54:47 +0530

    package update

---
+++ lib/pages/application/bloc/application_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'application_event.dart';
+part 'application_state.dart';
+
+class ApplicationBloc extends Bloc<ApplicationEvent, ApplicationState> {
+  ApplicationBloc() : super(ApplicationInitial()) {
+    on<ApplicationEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
