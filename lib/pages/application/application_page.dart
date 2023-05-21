7d6a63e77b4b63b418861b932799dadba85c0c13
shinigamimukund <shinigamimukund@gmail.com>
2023-05-21 18:08:24 +0530

    signin to application connected

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -13,7 +13,7 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
-  final int _index = 0;
+  // final int _index = 0;
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ApplicationBloc, ApplicationState>(
@@ -60,88 +60,7 @@ class _ApplicationState extends State<Application> {
                 selectedItemColor: AppColor.primaryElement,
                 unselectedItemColor: AppColor.primaryFourElementText,
                 elevation: 0,
-                items: [
-                  BottomNavigationBarItem(
-                    label: "Home",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset("assets/icons/home.png"),
-                    ),
-                    activeIcon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset(
-                        "assets/icons/home.png",
-                        color: AppColor.primaryElement,
-                      ),
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    label: "Search",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset("assets/icons/search.png"),
-                    ),
-                    activeIcon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset(
-                        "assets/icons/search2.png",
-                        color: AppColor.primaryElement,
-                      ),
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    label: "Course",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset("assets/icons/play-circle1.png"),
-                    ),
-                    activeIcon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset(
-                        "assets/icons/play-circle1.png",
-                        color: AppColor.primaryElement,
-                      ),
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    label: "Chat",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset("assets/icons/message-circle.png"),
-                    ),
-                    activeIcon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset(
-                        "assets/icons/message-circle.png",
-                        color: AppColor.primaryElement,
-                      ),
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    label: "Profile",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset("assets/icons/person.png"),
-                    ),
-                    activeIcon: SizedBox(
-                      width: 15.w,
-                      height: 15.h,
-                      child: Image.asset(
-                        "assets/icons/person.png",
-                        color: AppColor.primaryElement,
-                      ),
-                    ),
-                  ),
-                ],
+                items: bottomTab,
               ),
             ),
           ),
--- lib/pages/application/widgets/application_widget.dart
+++ lib/pages/application/widgets/application_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../common/entities/values/colors.dart';
 
 ///dynamic pages
 Widget buildPage(int index) {
@@ -21,3 +24,86 @@ Widget buildPage(int index) {
   ];
   return widget[index];
 }
+
+var bottomTab = [
+  BottomNavigationBarItem(
+    label: "Home",
+    icon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset("assets/icons/home.png"),
+    ),
+    activeIcon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset(
+        "assets/icons/home.png",
+        color: AppColor.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Search",
+    icon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset("assets/icons/search.png"),
+    ),
+    activeIcon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset(
+        "assets/icons/search2.png",
+        color: AppColor.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Course",
+    icon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset("assets/icons/play-circle1.png"),
+    ),
+    activeIcon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset(
+        "assets/icons/play-circle1.png",
+        color: AppColor.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Chat",
+    icon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset("assets/icons/message-circle.png"),
+    ),
+    activeIcon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset(
+        "assets/icons/message-circle.png",
+        color: AppColor.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Profile",
+    icon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset("assets/icons/person.png"),
+    ),
+    activeIcon: SizedBox(
+      width: 15.w,
+      height: 15.h,
+      child: Image.asset(
+        "assets/icons/person.png",
+        color: AppColor.primaryElement,
+      ),
+    ),
+  ),
+];
